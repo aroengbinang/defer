eb0b6123d387d5ed45a95af8518d86c7e0b1d45e
aroengbinang <65850876+aroengbinang@users.noreply.github.com>
2020-05-24 18:42:16 +0700

    Delete disq.js

--- disq.js
+++
@@ -1,7 +0,0 @@
-var disqus_shortname = 'aroengbinang';
-var disqus_blogger_current_url = "<data:blog.canonicalUrl/>";
-if (!disqus_blogger_current_url.length) {
-disqus_blogger_current_url = "<data:blog.url/>";
-}
-var disqus_blogger_homepage_url = "<data:blog.homepageUrl/>";
-var disqus_blogger_canonical_homepage_url = "<data:blog.canonicalHomepageUrl/>";
