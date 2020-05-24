9415ab80cbed73cf791c0a8434e00f82f95143c9
aroengbinang <65850876+aroengbinang@users.noreply.github.com>
2020-05-24 17:33:43 +0700

    Update disq.js

--- disq.js
+++ disq.js
@@ -1,25 +1,25 @@
-var disqus_shortname="aroengbinang";
-var disqus_url = disqus_blogger_current_url;
+var disqus_shortname = &#39;aroengbinang&#39;;
+var disqus_blogger_current_url = &quot;<data:blog.canonicalUrl/>&quot;;
+if (!disqus_blogger_current_url.length) {
+disqus_blogger_current_url = &quot;<data:blog.url/>&quot;;
+}
+var disqus_blogger_homepage_url = &quot;<data:blog.homepageUrl/>&quot;;
+var disqus_blogger_canonical_homepage_url = &quot;<data:blog.canonicalHomepageUrl/>&quot;;
 
-(function () {
-    "use strict";
-    var get_comment_block = function () {
-        var block = document.getElementById('comments');
-        if (!block) {
-            block = document.getElementById('disqus-blogger-comment-block');
-        }
-        return block;
-    };
-    var comment_block = get_comment_block();
-    if (!!comment_block) {
-        var disqus_div = document.createElement('div');
-        disqus_div.id = 'disqus_thread';
-        comment_block.innerHTML = '';
-        comment_block.appendChild(disqus_div);
-        comment_block.style.display = 'block';
-        var dsq = document.createElement('script');
-        dsq.async = true;
-        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-        (document.getElementsByTagName('head')[0] || document.body).appendChild(dsq);
-    }
+(function() {
+    var bloggerjs = document.createElement(&#39;script&#39;);
+    bloggerjs.type = &#39;text/javascript&#39;;
+    bloggerjs.async = true;
+    bloggerjs.src = &#39;//&#39; + disqus_shortname + &#39;.disqus.com/blogger_item.js&#39;;
+    (document.getElementsByTagName(&#39;head&#39;)[0] || document.getElementsByTagName(&#39;body&#39;)[0]).appendChild(bloggerjs);
 })();
+
+<script type='text/javascript'>
+(function() {
+var bloggerjs = document.createElement(&#39;script&#39;);
+bloggerjs.type = &#39;text/javascript&#39;;
+bloggerjs.async = true;
+bloggerjs.src = &#39;//&#39; + disqus_shortname + &#39;.disqus.com/blogger_index.js&#39;;
+(document.getElementsByTagName(&#39;head&#39;)[0] || document.getElementsByTagName(&#39;body&#39;)[0]).appendChild(bloggerjs);
+})();
+</script>
