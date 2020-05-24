65565b5e4507f47c7150c788159c059c34dd8c15
aroengbinang <65850876+aroengbinang@users.noreply.github.com>
2020-05-24 18:43:05 +0700

    Create disq.js

---
+++ disq.js
@@ -0,0 +1,7 @@
+var disqus_shortname = 'aroengbinang';
+var disqus_blogger_current_url = "<data:blog.canonicalUrl/>";
+if (!disqus_blogger_current_url.length) {
+disqus_blogger_current_url = "<data:blog.url/>";
+}
+var disqus_blogger_homepage_url = "<data:blog.homepageUrl/>";
+var disqus_blogger_canonical_homepage_url = "<data:blog.canonicalHomepageUrl/>";
