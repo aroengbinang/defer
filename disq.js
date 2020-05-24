ac2bdb2c5a65733519fc3eb228bcd9147451ea1d
aroengbinang <65850876+aroengbinang@users.noreply.github.com>
2020-05-24 18:32:59 +0700

    Update disq.js

--- disq.js
+++ disq.js
@@ -1,7 +1,7 @@
-var disqus_shortname = &#39;aroengbinang&#39;;
-var disqus_blogger_current_url = &quot;<data:blog.canonicalUrl/>&quot;;
+var disqus_shortname = 'aroengbinang';
+var disqus_blogger_current_url = "<data:blog.canonicalUrl/>";
 if (!disqus_blogger_current_url.length) {
-disqus_blogger_current_url = &quot;<data:blog.url/>&quot;;
+disqus_blogger_current_url = "<data:blog.url/>";
 }
-var disqus_blogger_homepage_url = &quot;<data:blog.homepageUrl/>&quot;;
-var disqus_blogger_canonical_homepage_url = &quot;<data:blog.canonicalHomepageUrl/>&quot;;
+var disqus_blogger_homepage_url = "<data:blog.homepageUrl/>";
+var disqus_blogger_canonical_homepage_url = "<data:blog.canonicalHomepageUrl/>";
